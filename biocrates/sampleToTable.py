be5c129c73845b9812a08aaa05b325a146f0d4d8
Alfie Abdul-Rahman <alfie.abdulrahman@gmail.com>
2015-06-30 17:18:04 +0100

    Fixed bug in grouping the plates and rawDataFileNames.

--- biocrates/sampleToTable.py
+++ biocrates/sampleToTable.py
@@ -2,38 +2,46 @@ __author__ = 'alfie'
 
 import sys, os
 from bs4 import BeautifulSoup as Soup
+from collections import defaultdict
 
 
-def generatePolarityAttrsDict(plate, polarity, myAttrs, rawDataFileName, mydict):
+def generatePolarityAttrsDict(plate, polarity, myAttrs, myMetabolites, mydict):
+    usedop = plate.get('usedop')
+    platebarcode = plate.get('platebarcode')
     injection = plate.find_all('injection', {'polarity': polarity})
     if (len(injection) > 0):
         for pi in injection:
-            rawDataFileName.append(pi.get('rawdatafilename'))
+            myAttrList = []
+            myMetabolitesList = []
             for p in pi.find_all('measure'):
                 myrdfname = p.find_parent('injection').get('rawdatafilename').split('.')[0]
-                usedop = p.find_parent('plate').get('usedop')
-                platebarcode = p.find_parent('plate').get('platebarcode')
                 for attr, value in p.attrs.iteritems():
                     if attr != 'metabolite':
                         mydict[p.get('metabolite') + '-' + myrdfname + '-' + attr + '-' + polarity.lower() + '-' + usedop + '-' + platebarcode] = value
-                        myAttrs.append(attr)
-    return (myAttrs, rawDataFileName, mydict)
+                        if attr not in myAttrList:
+                            myAttrList.append(attr)
+                myMblite = p.get('metabolite')
+                if myMblite not in myMetabolitesList:
+                    myMetabolitesList.append(myMblite)
+            # it is assume that the rawdatafilename is unique in each of the plate grouping and polarity
+            myAttrs[pi.get('rawdatafilename').split('.')[0]] = myAttrList
+        myMetabolites[usedop + '-' + platebarcode + '-' + polarity.lower()] = myMetabolitesList
+    return (myAttrs, mydict)
 
 
-def generateAttrsDict(plates):
-    posAttrs = []
-    negAttrs = []
-    posRawDataFileName = []
-    negRawDataFileName = []
+def generateAttrsDict(plate):
+    # using dictionaries of lists
+    posAttrs = defaultdict(list)
+    negAttrs = defaultdict(list)
+    posMetabolites = defaultdict(list)
+    negMetabolites = defaultdict(list)
     mydict = {}
-
-    for plate in plates:
-        posAttrs, posRawDataFileName, mydict = generatePolarityAttrsDict(plate, 'POSITIVE', posAttrs, posRawDataFileName, mydict)
-        negAttrs, negRawDataFileName, mydict = generatePolarityAttrsDict(plate, 'NEGATIVE', posAttrs, posRawDataFileName, mydict)
-    return (posAttrs, negAttrs, posRawDataFileName, negRawDataFileName, mydict)
+    posAttrs, mydict = generatePolarityAttrsDict(plate, 'POSITIVE', posAttrs, posMetabolites, mydict)
+    negAttrs, mydict = generatePolarityAttrsDict(plate, 'NEGATIVE', negAttrs, negMetabolites, mydict)
+    return (posAttrs, negAttrs, posMetabolites, negMetabolites, mydict)
 
 
-def writeOutToFile(plate, polarity, usedop, platebarcode, output_dir, rawDataFileName, uniqueAttrs, uniqueMetaboliteIdentifiers, mydict):
+def writeOutToFile(plate, polarity, usedop, platebarcode, output_dir, uniqueAttrs, uniqueMetaboliteIdentifiers, mydict):
     pos_injection = plate.find_all('injection', {'polarity': polarity})
     if (len(pos_injection) > 0):
         filename = usedop + '-' + platebarcode + '-' + polarity.lower() + '-maf.txt'
@@ -41,15 +49,15 @@ def writeOutToFile(plate, polarity, usedop, platebarcode, output_dir, rawDataFil
         with open(os.path.join(output_dir, filename), 'w') as file_handler:
             # writing out the header
             file_handler.write('Sample ID')
-            for rawdatafname in rawDataFileName:
-                for myattr in uniqueAttrs:
-                    file_handler.write('\t' + rawdatafname.split('.')[0] + '[' + myattr + ']')
+            for ua in uniqueAttrs:
+                for myattr in uniqueAttrs[ua]:
+                    file_handler.write('\t' + ua + '[' + myattr + ']')
             # now the rest of the rows
-            for mymetabolite in uniqueMetaboliteIdentifiers:
-                file_handler.write('\n' + mymetabolite)
-                for rawdatafname in rawDataFileName:
-                    for myattr in uniqueAttrs:
-                        mykey = mymetabolite + '-' + rawdatafname.split('.')[0] + '-' + myattr + '-' + polarity.lower() + '-' + usedop + '-' + platebarcode
+            for myMetabolite in uniqueMetaboliteIdentifiers[usedop + '-' + platebarcode + '-' + polarity.lower()]:
+                file_handler.write('\n' + myMetabolite)
+                for ua in uniqueAttrs:
+                    for myattr in uniqueAttrs[ua]:
+                        mykey = myMetabolite + '-' + ua + '-' + myattr + '-' + polarity.lower() + '-' + usedop + '-' + platebarcode
                         if mykey in mydict:
                             file_handler.write('\t' + mydict[mykey])
                         else:
@@ -61,32 +69,26 @@ def parseSample(file):
     folder_name = 'output'
     output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), folder_name)
 
+    # create the output directory if it does not exists
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     file = sys.argv[1]
+    # open and read up the file
     handler = open(file).read()
     soup = Soup(handler)
 
-    # extracting the unique metabolite identifiers
-    metaboliteList = []
-    metabolite = soup.find_all('metabolite')
-    for met in metabolite:
-        metaboliteList.append(met.get('identifier'))
-    uniqueMetaboliteIdentifiers = list(set(metaboliteList))
-
-    # extracting the distinct column labels and the rawdatafilename
+    # get all the plates
     plates = soup.find_all('plate')
-    posAttrs, negAttrs, posRawDataFileName, negRawDataFileName, mydict = generateAttrsDict(plates)
-    uniquePosAttrs = list(set(posAttrs))
-    uniqueNegAttrs = list(set(negAttrs))
-
-    # creating the sample tab files
     for plate in plates:
         usedop = plate.get('usedop')
         platebarcode = plate.get('platebarcode')
-        writeOutToFile(plate, 'POSITIVE', usedop, platebarcode, output_dir, posRawDataFileName, uniquePosAttrs, uniqueMetaboliteIdentifiers, mydict)
-        writeOutToFile(plate, 'NEGATIVE', usedop, platebarcode, output_dir, negRawDataFileName, uniqueNegAttrs, uniqueMetaboliteIdentifiers, mydict)
+        # extracting the the distinct column labels, metabolites, and rawdatafilename
+        # collect the data into a dictionary
+        posAttrs, negAttrs, posMetabolites, negMetabolites, mydict = generateAttrsDict(plate)
+        # and start creating the sample tab files
+        writeOutToFile(plate, 'POSITIVE', usedop, platebarcode, output_dir, posAttrs, posMetabolites, mydict)
+        writeOutToFile(plate, 'NEGATIVE', usedop, platebarcode, output_dir, negAttrs, negMetabolites, mydict)
 
 
 if __name__ == "__main__":
